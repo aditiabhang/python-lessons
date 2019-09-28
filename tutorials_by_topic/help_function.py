b619b0e8fa8b8eea19ae07de8cc20c7f2ca3c253
sanket lawangare <srlawang@us.ibm.com>
2019-09-28 09:01:32 -0500

    removed old dir

--- tutorials_by_topic/help_function.py
+++
@@ -1,9 +0,0 @@
-# greeting = "Hello World!"
-
-# to lookup on how to use a method 
-# print(dir(greeting))
-
-# print(help(str))
-# print(help(str.strip))
-
-
--- tutorials_by_topic/strings/slice_strings.py
+++
@@ -1,9 +0,0 @@
-url = "https://www.google.com"
-
-# string[start:end:step]
-
-# reading elements in a reverse order
-print(url[::-1])
-
-# print out just google
-print(url[12:-4])
